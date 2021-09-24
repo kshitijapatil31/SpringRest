2a26428f415a8d58d48ef40e028469cf2922e76e
Kshitija Patil <patilkshitija2@gmail.com>
2021-09-24 22:29:37 +0530

    [kshitija]:rest call to show say hello mark

--- src/main/java/com/example/SpringRestProject/controller/HelloRestController.java
+++ src/main/java/com/example/SpringRestProject/controller/HelloRestController.java
@@ -2,6 +2,8 @@ package com.example.SpringRestProject.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
@@ -11,6 +13,12 @@ public class HelloRestController {
 	@RequestMapping(value= {"","/","/hello"})
 	public String hello() {
 		return "Hello from Bridgelabz";
+		
+	}
+	
+	@RequestMapping(value= {"/query"},method=RequestMethod.GET)
+	public String SayHello(@RequestParam(value="fName") String fName,@RequestParam(value="lName")String lName) {
+		return "Hello"+fName+" "+lName+"";
 		
 	}
 }
