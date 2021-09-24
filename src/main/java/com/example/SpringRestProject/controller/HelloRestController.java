2da3327cd362a8f584b11bda697363d905607e57
Kshitija Patil <patilkshitija2@gmail.com>
2021-09-24 23:04:15 +0530

    [kshitija]:POST Request Method and pass first name and last name in the Body

--- src/main/java/com/example/SpringRestProject/controller/HelloRestController.java
+++ src/main/java/com/example/SpringRestProject/controller/HelloRestController.java
@@ -3,9 +3,13 @@ package com.example.SpringRestProject.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.SpringRestProject.model.User;
 
 
 @Controller
@@ -27,5 +31,9 @@ public class HelloRestController {
 	public String helloPathVarible(@PathVariable String name) {
 		return name;
 		
+	}
+	@PostMapping("/post")
+	public String helloPost(@RequestBody User user) {
+		return user.getFirstName()+" "+user.getLastName();
 	}
 }
